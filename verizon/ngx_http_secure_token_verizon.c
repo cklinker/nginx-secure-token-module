413ae3b0bd0bc64317d0a3d94e053a09f36b033e
Craig Klinker <cklinker@rzware.com>
2019-02-27 22:57:10 -0700

    verizon

--- verizon/ngx_http_secure_token_verizon.c
+++ verizon/ngx_http_secure_token_verizon.c
@@ -111,7 +111,9 @@ ngx_secure_token_verizon_get_var(
 	}
 
 	v->data = p;
-	signature = ngx_string(&l_token);
+	int ptr = ngx_sprintf(signature.data, l_token);
+
+//	signature = ngx_string(&l_token);
 
 	p = ngx_encode_base64(p, &signature);
 	*p = '\0';
